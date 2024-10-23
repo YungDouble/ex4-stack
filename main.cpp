5913ef9b1642436d7f965aec1be9acf2e7695718
ezkl <therealdavos@gmail.com>
2024-10-23 00:03:25 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+/**
+* Programmer: Davos DeHoyos
+ * Project number: 4
+ * Project Desc: Stack class implementation using linked list
+ * Course: COSC 2436 PF III Data Structures
+ * Date:   10/22/2024
+ */
