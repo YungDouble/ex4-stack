f1c9d57ffc1187223b38a0b6da731fcf79b28de9
ezkl <therealdavos@gmail.com>
2024-10-26 18:10:13 -0500

    redefined push as bool instead of void

--- stack.h
+++ stack.h
@@ -6,12 +6,22 @@ class Stack {
     Stack();
     ~Stack();
 
-    void push(int data);
+    bool push(int data);
     int pop();
-    int peek();
-    bool isEmpty();
+    int peek() const;
+    bool isEmpty () const;
 
     private:
+    // A node class to represent each element in the stack
+    class Node {
+        public:
+            int data;
+            Node* next;
+
+            Node(int data) : data(data), next(nullptr) {}   // Constructor to initialize the node
+    };
+
+    Node* top;
 };
 
 #endif //STACK_H
